af1104183a2e9c5bb3267a380695c4919f773abb
sobatha <natsuki.yokoizo.ny@gmail.com>
2022-08-22 15:36:18 +0000

    incomeフォーム追加、決算ビュー追加

--- kakeibo/urls.py
+++ kakeibo/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     path("", views.index, name='index'),
     path("<int:year>/<int:month>/", views.month, name='month'),
     path("<int:year>/", views.year, name="year"),
+    path("settlement/<int:year>/<int:month>", views.settlement, name='settlement'),
     path("stock/", views.stock, name='stock'),
-    path("forms/", views.PaymentCreate, name='payment_create'),
+    path("payment_forms/", views.PaymentCreate, name='payment_create'),
+    path("income_forms/", views.IncomeCreate, name='income_create'),
     path("delete/<int:pk>",views.payment_delete,name="payment_delete"),
     path("update/<int:pk>", views.payment_update, name="payment_update")
 ]
